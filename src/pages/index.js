526283eaa525d6f982222d472ce9f358c6e10948
LeeSure <leesurehx@gmail.com>
2018-09-10 16:32:14 +0800

    update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design ande code React Apps</h1>
     <p>测试一下这个好不好用</p>
-    <p>Tryasd;kfjasdj;lgkjasdlkgjas;ldkjga;lskdjg;laksdjglaksdjg;laksjdg;laksjdg;lkasd</p>
+    <p>更新一下</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
