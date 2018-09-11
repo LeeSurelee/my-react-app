5e4773106787f8de70e35f291257a944e39f49a4
LeeSure <leesurehx@gmail.com>
2018-09-11 11:28:51 +0800

    Section2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design ande code React Apps</h1>
-    <p>测试一下这个好不好用</p>
-    <p>更新一下</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learn to design ande code React Apps</h1>
+      <p>测试一下这个好不好用</p>
+      <p>更新一下</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
